192503183c8f652755a8a9b1c6f5f1896c2ca46a
Collin Shuey <cjs019@lvc.edu>
2024-05-19 20:46:54 -0400

    first_day

--- tuneshare/core/models.py
+++ tuneshare/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+# Gets current user model and stores it in User, the default one that comes with Django
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True,max_length=500)
+    profileimg = models.ImageField(upload_to='profile_images',default='blank-profile-picture.png')
+    location = models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.user.get_username()
