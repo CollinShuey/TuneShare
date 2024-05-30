045ff754584e58254573f7beb04f4404b0714284
Collin Shuey <cjs019@lvc.edu>
2024-05-29 21:09:27 -0400

    asdfg

--- tuneshare/core/views.py
+++ tuneshare/core/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User,auth
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 # Create your views here.
-
+@login_required(login_url='signin')
 def index(request):
     return render(request,'index.html')
 
@@ -26,6 +28,14 @@ def signup(request):
             else:
                 user = User.objects.create_user(username=username,email=email,password=password)
                 user.save()
+
+                # log user in and redirect to settings page
+
+                #create a Profile object for the new user
+                # user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user,id_user=user.id)
+                new_profile.save()
+                return redirect('signup')
         
         else:
             messages.info(request,'Password Not Matching')
@@ -33,3 +43,24 @@ def signup(request):
 
     else:
         return render(request,'signup.html')
+    
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('signin')
+        
+    else:
+        return render(request,'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
